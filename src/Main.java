d26abb7cac214cc61ee1d331b75d978c6d868605
Tsumbedzo Matloga <tsumbedzomatloga@gmail.com>
2025-11-20 08:25:09 +0200

    Full project

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        TaskScheduler scheduler = new TaskScheduler();
+        scheduler.addTask("Fix bug", 1);
+        scheduler.addTask("Write documentation", 4);
+        scheduler.addTask("Push to GitHub",2);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Processing Task by Priority:");
+        while (!scheduler.isEmpty()) {
+            System.out.println("Done: " + scheduler.processNextTask());
         }
+
+        Graph graph = new Graph();
+        String[] nodes = {"A", "B", "C", "D", "E"};
+
+        for (String n : nodes) graph.addNode(n);
+
+        graph.addEdge("A", "B");
+        graph.addEdge("B", "C");
+        graph.addEdge("C", "D");
+        graph.addEdge("D", "E");
+
+        System.out.println("\nShortest path from A to D:");
+        System.out.println(PathFinder.bfsShortestPath(graph, "A", "D"));
     }
 }
